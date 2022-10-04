6883933173a7aabd84339c2b614daae21330dfb1
levimorse <63205032+levimorse@users.noreply.github.com>
2022-10-03 18:10:03 -0600

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -10,6 +10,8 @@ from random import choice
 from WordleDictionary import FIVE_LETTER_WORDS
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
+
+
 def wordle():
 
     def enter_action(s):
@@ -18,9 +20,39 @@ def wordle():
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
 
+def colorCheck(word, guess):
+    guessScore = ["white", "white", "white", "white", "white"]
+    wordTemp = word
+    index = 0
+    # Checking for GREEN
+    for char in wordTemp:
+        if char == guess[index]:
+            guessScore[index] = "green"
+            wordTemp = wordTemp[:index] + "_" + wordTemp[index + 1:]
+            print(wordTemp + " green check")
+        index = index + 1
+    
+    # Checking for YELLOW
+    index = 0
+    for char in wordTemp:
+        if guess.find(char) >= 0:
+            guessScore[index] = "yellow"
+            wordTemp = wordTemp[:index] + "_" + wordTemp[index + 1:]
+            print(wordTemp + " yellow check")
+        index = index+ 1
+
+    index = 0
+    for color in guessScore:
+        if color == "white":
+            guessScore[index] = "gray"
+        index = index + 1
+    return guessScore
+
+
 # Startup code
 
 if __name__ == "__main__":
-    wordle()
+    #wordle()
     TheWordle = choice(FIVE_LETTER_WORDS)
     print(TheWordle)
+    print(colorCheck("phage", "gauge"))
